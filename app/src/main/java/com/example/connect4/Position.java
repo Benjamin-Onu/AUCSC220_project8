79bac3c20f2eed2cb57624d6ccd0cfc15725ae22
Benjamin <benjaminonuzurike963@gmail.com>
2023-11-09 14:25:31 -0700

    Position object class

---
+++ app/src/main/java/com/example/connect4/Position.java
@@ -0,0 +1,32 @@
+package com.example.connect4;
+
+public class Position {
+    private int row;
+    private int column;
+    private String player;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public Position(int row, int column, String player){
+        this.row = row;
+        this.column = column;
+        this.player = player;
+    }
+
+    @Override
+    public String toString(){
+        String output = "(" + row + "," +
+                column + "," + player + ")";
+        return output;
+    }
+}

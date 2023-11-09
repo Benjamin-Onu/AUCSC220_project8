734e1866bcaa90daca80ad41db20c6d6ccbd6f3e
e3bomi <tamaraue@ualberta.ca>
2023-11-09 15:39:57 -0700

    still waiting for our prototype to be pushed so I can use the buttons from there

---
+++ app/src/main/java/com/example/connect4/HomePageActivity.java
@@ -0,0 +1,28 @@
+
+
+public class HomePageActivity extends AppCompatActivity{
+    Button rules;
+    Button settings;
+
+    /**
+     * This function will run when the “view rule” button is clicked. There will be a textview
+     * containing the rules that will be hidden and this function will enable that textview.
+     */
+    protected void displayRules(){
+
+    }
+    /**
+     * This function will take the user to the game page for human vs human
+     */
+    protected void goToHHGamePage(){
+
+    }
+
+    /**
+     * This function will take the user to the game page for human vs AI
+     */
+    protected void goToHAGamePage(){
+
+    }
+
+}

93eb2dab7c252140f3ed065d898742cece6b36de
Weijun4 <wfeng4@ualberta.ca>
2023-11-10 05:52:26 +0800

    several initializations & a new mothod - decideWhoGoesFirst

--- app/src/main/java/com/example/connect4/HumanvsHuman.java
+++ app/src/main/java/com/example/connect4/HumanvsHuman.java
@@ -1,7 +1,25 @@
 package com.example.connect4;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import java.util.Random;
 
 public class HumanvsHuman {
+    private String player1;
+    private String player2;
+    private String winner;
+    private String turn;
+    private Model game = new Model();
+    private Random rand = new Random();
+
+    private String decideWhoGoesFirst(){
+        int turnNum = rand.nextInt(2); //turnNum → 0/1 → player 1/2 goes first
+        if (turnNum == 0){
+            turn = "player 1";
+        }
+        else{
+            turn = "player 2";
+        }
+        return turn;
+    }
+
+
 }

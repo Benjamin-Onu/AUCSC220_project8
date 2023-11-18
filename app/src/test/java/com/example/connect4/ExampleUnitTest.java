4e1d4eacffab3ac8deef0f7d170104463a163c90
Benjamin <benjaminonuzurike963@gmail.com>
2023-11-18 11:24:27 -0700

    Test case for Vertical count

--- app/src/test/java/com/example/connect4/ExampleUnitTest.java
+++ app/src/test/java/com/example/connect4/ExampleUnitTest.java
@@ -14,4 +14,23 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    public void countHorizontally(){
+
+
+    }
+
+    @Test
+    public void TestcountVertically(){
+        Model game = new Model();
+        Position[][] testBoard = new Position[5][5];
+        testBoard[4][0] = new Position(4, 0, "AI");
+        testBoard[3][0] = new Position(4, 0, "AI");
+        testBoard[2][0] = new Position(4, 0, "AI");
+        testBoard[1][0] = new Position(4, 0, "AI");
+        game.board = testBoard;
+
+        int count = game.countConsecutivePlayerSpotsVertically("AI");
+        assertEquals(count, 4);
+    }
 }

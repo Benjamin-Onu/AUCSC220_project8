8cb8ec2d6794572a7c45f7699a58461ab5558f7c
e3bomi <tamaraue@ualberta.ca>
2023-11-23 15:43:43 -0700

    Pop up instructions

--- app/src/main/java/com/example/connect4/MainActivity.java
+++ app/src/main/java/com/example/connect4/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.connect4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button instructions ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        instructions = (Button) findViewById(R.id.instructions);
+        instructions.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                openInstructions();
+            }
+
+
+        });
     }
+
+    private void openInstructions() {
+        Intent popupInstructions = new Intent(MainActivity.this, PopUpInstructions.class);
+        startActivity(popupInstructions);
+    }
+
+
 }

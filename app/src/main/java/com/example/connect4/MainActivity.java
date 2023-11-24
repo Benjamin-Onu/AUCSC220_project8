ce5e08b736cbd528effad442b5141febcdec48b5
Benjamin <benjaminonuzurike963@gmail.com>
2023-11-23 17:11:02 -0700

    .

--- app/src/main/java/com/example/connect4/MainActivity.java
+++ app/src/main/java/com/example/connect4/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
 
         instructions = (Button) findViewById(R.id.instructions);
         instructions.setOnClickListener(new View.OnClickListener(){

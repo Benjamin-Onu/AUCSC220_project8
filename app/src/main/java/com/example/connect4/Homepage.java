da45267de527d1952b014d35b2a07ada875c6120
e3bomi <tamaraue@ualberta.ca>
2023-11-28 14:45:34 -0700

    made a few modifications to these files.

--- app/src/main/java/com/example/connect4/Homepage.java
+++ app/src/main/java/com/example/connect4/Homepage.java
@@ -2,19 +2,42 @@ package com.example.connect4;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class Homepage extends AppCompatActivity {
-    //Button rules;
+
     //Button settings;
+    Button instructions;//was formerly the rules button
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        instructions = (Button) findViewById(R.id.instructions);
+        instructions.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
 
+                openInstructions();
+            }
 
-    /**
-     * This function will run when the “view rule” button is clicked. There will be a textview
-     * containing the rules that will be hidden and this function will enable that textview.
-     */
-    protected void displayRules(){
-
+        });
     }
+
+    private void openInstructions() {
+        Intent popupInstructions = new Intent(Homepage.this, PopUpInstructions.class);
+        startActivity(popupInstructions);
+    }
+
+//    /**
+//     * This function will run when the “view rule” button is clicked. There will be a textview
+//     * containing the rules that will be hidden and this function will enable that textview.
+//     */
+//    protected void displayRules(){
+//
+//    }
     /**
      * This function will take the user to the game page for human vs human
      */

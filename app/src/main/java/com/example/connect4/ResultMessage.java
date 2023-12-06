136b46ecb8ead24afc402448acc518e9485bf19d
e3bomi <tamaraue@ualberta.ca>
2023-12-06 11:27:24 -0700

    display winning message file started but I am focusing on how to connect it to the end of our gameplay loop in each various mode. Might have to create 2 different files to represent the various winners(AI, P1,P2,nobody) but would like to expand more on doing this first

--- app/src/main/java/com/example/connect4/ResultMessage.java
+++ app/src/main/java/com/example/connect4/ResultMessage.java
@@ -3,6 +3,9 @@ package com.example.connect4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
 
 public class ResultMessage extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class ResultMessage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_message);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*1), (int)(height*1));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.BOTTOM;
+        params.x = 0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
     }
 }

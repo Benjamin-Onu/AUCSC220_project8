2e555b98ffbb755a19af1ee48d2cec37a1e68db9
e3bomi <tamaraue@ualberta.ca>
2023-12-06 10:15:36 -0700

    Modification on layout of main along with additional files that will be used to display winning message

---
+++ app/src/main/java/com/example/connect4/ResultMessage.java
@@ -0,0 +1,14 @@
+package com.example.connect4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_message);
+    }
+}
